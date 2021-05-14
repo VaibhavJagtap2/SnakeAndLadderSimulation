50de2edcf1e7dd5c25ff6dae07b54cd2d36b8e86
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-14 23:17:45 +0530

    Player Rolling A Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,10 +6,19 @@ public class SnakeAndLadder {
 	
 	static final int STARTING_POSITION=0;
 	
+	public void DiceRoll() {
+		Random random = new Random();
+		int DICE = (random.nextInt(6)+1);
+	
+		System.out.println("Roll Dice :"+DICE);
+	}
+	
+	
+	
 	public static void main(String[] args) {
 
 	System.out.println("Welcome To Snake And Ladder Game");
-	SnakeAndLadder player = new SnakeAndLadder();
-	
+	SnakeAndLadder player1= new SnakeAndLadder();
+	player1.DiceRoll(); 
 }
 }
