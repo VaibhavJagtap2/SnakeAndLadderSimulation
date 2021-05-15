fd9dd1098223d9df07fbeedfd02d9ac4d58162ad
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-15 13:35:50 +0530

    Add Dice Throw To Win Game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -12,13 +12,16 @@ public class SnakeAndLadder {
 		
 		Random random = new Random();
 		int playerPosition = STARTING_POSITION;
+		String task;
+		int Total_Dice_Count = 0;
 		
 		System.out.println("Player Start Playing The Game");
 		
 		while(playerPosition < ENDING_POSITION) {
 			
 		int DICE = (random.nextInt(6)+1);
-	
+		Total_Dice_Count++;
+		
 		int action = random.nextInt(3);
 		
 		System.out.println("Roll Dice :"+DICE);
@@ -26,20 +29,24 @@ public class SnakeAndLadder {
 			if(action == 1 && (playerPosition + DICE <= ENDING_POSITION)){
 				
 			playerPosition = playerPosition + DICE;
+			task = "Ladder";
 			System.out.println("Player Moves Towards Ladder");
 			}
 			
 			else if(action == 2 && (playerPosition - DICE >= STARTING_POSITION)) {
 		    	 playerPosition = playerPosition - DICE;
+		    	 task = "Snake";
 		    	 System.out.println("Player Moves Towards Snake");
 		    	 
 		}
 		     else {
+		    	 	task = "No Play";
 		    	 	System.out.println("No Play");
 		     }
-			System.out.println("Dice : "+DICE+"  | playerPosition : "+playerPosition);
+			System.out.println(" | Dice : "+DICE+" for "+task+" and playerPosition : "+playerPosition+" | ");
 		}
 		
+			System.out.println("Total Count of Dice Throws : "+Total_Dice_Count);
 		
 }
   public static void main(String[] args) {
